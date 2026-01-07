23902fc142572a6eabda1735b5eea0c145aa488d
Gautam Rao <fcbayer@gmail.com>
2026-01-06 23:26:44 -0800

    Python class for De Anza college for week 1. Only contains P1 and P3

---
+++ De Anza Python Class/Input_P3.py
@@ -0,0 +1,26 @@
+# Input_P3.py
+# Gautam Rao
+# 1//06/26
+# Python 3.13.9
+# Description: Program to take input in Python
+
+name = input("Please enter Your Name: ")                        # this is a string
+weightLbs = float(input ("Please enter Your weight in lbs: "))  # a float
+age = int (input ("Please enter your age (whole number): " ))   # an integer
+weightKg= weightLbs*0.453592
+title = "Human"
+
+print ("Hello", title, name, "your weight is")
+print ( weightLbs, "lbs")
+print ("which equals = %.2f" %weightKg, end=' ')                # end=' ' prevents new line
+print ("kilograms ")
+
+'''
+Please enter Your Name: Gautam Rao
+Please enter Your weight in lbs: 207.5
+Please enter your age (whole number): 37
+Hello Human Gautam Rao your weight is
+207.5 lbs
+which equals = 94.12 kilogram
+
+'''
---
+++ De Anza Python Class/output_P1.py
@@ -0,0 +1,40 @@
+# output_P1.py
+# Gautam Rao
+# 1/06/26
+# Python 3.13.9
+# Description: Program show's output in Python
+
+print('Hello World!')            # single quote
+print("Hello World!")            # double quote
+print("he\nllo")                 # \n insert a new line (same as pressing Enter)
+
+# VARIABLES are named storage locations for numbers, strings, lists
+# STRING is anything enclosed in quotes "Hello World", that's a string
+# NUMBER can be either a FLOAT (Ex: 9.3) or an INTEGER (Ex: 9.0)
+# LISTS are things like [1,2,3] or ["Alex", "Stoykov"]
+myName = "Gautam Rao"               # declare/initialize string variable myName
+Weight = 207.5                        # declare/initialize float variable Weight
+Age = 37                            # declare/initialize int variable Age
+Grades = [93.5,95.5]
+Name = ["Gautam", "Rao"]
+
+print ("Hello ", myName)
+print ("Your weight is ", Weight, "and your age is ", Age)
+print ("Your weight is %.2f and your age is %i" %(Weight, Age))
+print ("Lists: grades =",Grades,"nameZ=",Name)
+print ("This is how you print", end=' ')
+print ("on the same line")
+
+
+'''
+Hello World!
+Hello World!
+he
+llo
+Hello  Gautam Rao
+Your weight is  205 and your age is  37
+Your weight is 205.00 and your age is 37
+Lists: grades = [93.5, 95.5] nameZ= ['Gautam', 'Rao']
+This is how you print on the same line
+
+'''

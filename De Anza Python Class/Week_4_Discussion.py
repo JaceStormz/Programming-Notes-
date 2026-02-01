1c4b7ad7cdeb3213b652e37bebb8325b46df0b87
Gautam Rao <fcbayer@gmail.com>
2026-02-01 12:05:19 -0800

    Files to be saved as back up and Python discussion assignment

---
+++ De Anza Python Class/Week_4_Discussion.py
@@ -0,0 +1,30 @@
+# Week_4_Discussion.py
+# Gautam Rao
+# 1//23/26
+# Python 3.13.9
+# Program which print in loop as many times as the current hour.
+currenthour = int(input("Enter the current hour (0–23): "))
+
+for i in range(currenthour):
+    print(f"i = {i+1}")
+# the hours enter is 16 for 4pm 
+'''
+Enter the current hour (0–23): 16
+i = 1
+i = 2
+i = 3
+i = 4
+i = 5
+i = 6
+i = 7
+i = 8
+i = 9
+i = 10
+i = 11
+i = 12
+i = 13
+i = 14
+i = 15
+i = 16
+
+'''
